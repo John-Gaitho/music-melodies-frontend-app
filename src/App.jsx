7076c2b441af9614a148b161e47e70884c0232ee
John Gaitho <jgaitho016@gmail.com>
2025-01-31 22:29:11 +0300

    imports

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SongList from './components/SongList';
+import PlaylistList from './components/PlaylistList';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import SongForm from './components/SongForm';
+
