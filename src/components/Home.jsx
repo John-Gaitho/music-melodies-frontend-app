34aa7e72c0d63b0fa3146555319b3aa3e84cfa31
John Gaitho <jgaitho016@gmail.com>
2025-01-31 19:49:02 +0300

    home file

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Music Melodies! </h1>
+      <h1>Your go-to place for discovering and managing songs and playlists.</h1>
+    </div>
+  );
+}
+
+export default Home;
