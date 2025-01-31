514923d64d583d59329c0de8756e177d987a12b8
John Gaitho <jgaitho016@gmail.com>
2025-01-31 19:55:22 +0300

    navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+   
+      <ul>
+        <li>Home</li>
+        <li>Songs</li> 
+        <li>Add Songs</li>
+        <li>Playlists</li>
+      </ul>
+    
+  );
+}
+
+export default Navbar;
