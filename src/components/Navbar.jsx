d1a74a3f163e07890a6515c5d7754fbda2dfd006
John Gaitho <jgaitho016@gmail.com>
2025-01-31 22:20:51 +0300

    nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-   
+    <nav>
       <ul>
-        <li>Home</li>
-        <li>Songs</li> 
-        <li>Add Songs</li>
-        <li>Playlists</li>
+        <li><Link to="/Home">Home</Link></li>
+        <li><Link to="/">Songs</Link></li> 
+        <li><Link to="/add-song">Add Songs</Link></li>
+        <li><Link to="/playlists">Playlists</Link></li>
       </ul>
-    
+    </nav>
   );
 }
 
